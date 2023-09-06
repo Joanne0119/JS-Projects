d869e50fdbd0dec434d4e4e1530cad3d6c3ea84d
Joanne <liuchengen52033@gmail.com>
2023-09-06 20:21:47 +0800

    Almost finish

--- Calculator/script/calculator.js
+++ Calculator/script/calculator.js
@@ -1,114 +1,146 @@
-// const acBtn = document.querySelector('.js-ac-btn');
-// const postiveNegativeBtn = document.querySelector('.js-postive-negative-btn');
-// const percentBtn = document.querySelector('.js-percent-btn');
-// const divideBtn = document.querySelector('.js-divide-btn');
-// const sevenBtn = document.querySelector('.js-seven-btn');
-// const eightBtn = document.querySelector('.js-eight-btn');
-// const nineBtn = document.querySelector('.js-nine-btn');
-// const multiplyBtn = document.querySelector('.js-multiply-btn');
-// const fourBtn = document.querySelector('.js-four-btn');
-// const fiveBtn = document.querySelector('.js-five-btn');
-// const sixBtn = document.querySelector('.js-six-btn');
-// const minusBtn = document.querySelector('.js-minus-btn');
-// const oneBtn = document.querySelector('.js-one-btn');
-// const twoBtn = document.querySelector('.js-two-btn');
-// const threeBtn = document.querySelector('.js-three-btn');
-// const plusBtn = document.querySelector('.js-plus-btn');
-// const zeroBtn = document.querySelector('.js-zero-btn');
-// const dotBtn = document.querySelector('.js-dot-btn');
-// const equationBtn = document.querySelector('.js-equation-btn');
-
-// acBtn.addEventListener('click', () => {
-//   clear();
-// });
-// postiveNegativeBtn.addEventListener('click', () => {
-  
-// });
-// percentBtn.addEventListener('click', () => {
-//   calculatePrint('%');
-// });
-// divideBtn.addEventListener('click', () => {
-//   calculatePrint(' / ');
-// });
-// sevenBtn.addEventListener('click', () => {
-//   calculatePrint('7');
-// });
-// eightBtn.addEventListener('click', () => {
-//   calculatePrint('8');
-// });
-// nineBtn.addEventListener('click', () => {
-//   calculatePrint('9');
-// });
-// multiplyBtn.addEventListener('click', () => {
-//   calculatePrint(' * ');
-// });
-// fourBtn.addEventListener('click', () => {
-//   calculatePrint('4');
-// });
-// fiveBtn.addEventListener('click', () => {
-//   calculatePrint('5');
-// });
-// sixBtn.addEventListener('click', () => {
-//   calculatePrint('6');
-// });
-// minusBtn.addEventListener('click', () => {
-//   calculatePrint(' - ');
-// });
-// oneBtn.addEventListener('click', () => {
-//   calculatePrint('1');
-// });
-// twoBtn.addEventListener('click', () => {
-//   calculatePrint('2');
-// });
-// threeBtn.addEventListener('click', () => {
-//   calculatePrint('3');
-// });
-// plusBtn.addEventListener('click', () => {
-//   calculatePrint(' + ');
-// });
-// zeroBtn.addEventListener('click', () => {
-//   calculatePrint('0');
-// });
-// dotBtn.addEventListener('click', () => {
-//   calculatePrint('.');
-// });
-// equationBtn.addEventListener('click', () => {
-//   calculate();
-// });
-
-// let resultNumberString = '';
-// let resultNumber = 0;
-// function calculatePrint(num)
-// {
-//   resultNumberString += num;
-//   console.log(resultNumberString);
-// }
-
-// function calculate()
-// {
-//   resultNumber = Number(resultNumberString);
-//   calculatePrint(` = ${resultNumber}`);
-//   // console.log(resultNumber);
-// }
-
 const topPanle = document.querySelector('.js-top-panle');
-const buttons = document.querySelectorAll('button');
+const recordPanle = document.querySelector('.js-record-panle');
+const numberButtons = document.querySelectorAll('[data-number]');
+const operatorButtons = document.querySelectorAll('[data-operator]');
 
-console.log(buttons);
+console.log(numberButtons);
+console.log(operatorButtons);
 
-buttons.forEach((button) => {
+let calculation = [];
+let newCalculation = '';
+let newCalculationArrary = [];
+let newOperator;
+
+numberButtons.forEach((button) => {
   button.addEventListener('click', () => {
     calculate(button);
   })
 })
 
-let calculation = [];
+operatorButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    operation(button.value);
+  })
+})
+
+showOnTopPanle();
 
 function calculate(button)
 {
-  const buttonValue = button.textContent;
+  const buttonValue = button.value;
   
   calculation.push(buttonValue);
-  
+
+  GenNewCalculation();
+  showOnTopPanle();
   console.log(calculation);
+}
+
+function operation (operator)
+{
+  newCalculationArrary.push(newCalculation);
+  newCalculation = 0;
+  calculation = [];
+  newCalculationArrary.push(operator);
+  showOnRecordPanle();
+}
+
+function GenNewCalculation ()
+{
+  newCalculation = '';
+
+  calculation.forEach((value, index) => {
+    console.log(value);
+    console.log(index);
+    newCalculation = newCalculation + value;
+    console.log(newCalculation);
+  })
+
+}
+
+function showOnRecordPanle()
+{
+  let message = showNewCalculationArrary();
+  recordPanle.innerHTML = message;
+}
+
+function showOnTopPanle ()
+{
+  topPanle.innerHTML = newCalculation;
+}
+
+
+const acBtn = document.querySelector('.js-ac-btn');
+const negtivePostiveBtn = document.querySelector('.js-negtive-postive-btn');
+const percentBtn = document.querySelector('.js-percent-btn');
+const equationBtn = document.querySelector('.js-equation-btn');
+
+
+acBtn.addEventListener('click', () => {
+  clear();
+});
+negtivePostiveBtn.addEventListener('click', () => {
+  negtivePostive();
+});
+percentBtn.addEventListener('click', () => {
+  percent();
+});
+equationBtn.addEventListener('click', () => {
+  equation();
+});
+
+function clear()
+{
+  topPanle.innerHTML = '';
+  recordPanle.innerHTML = '';
+  calculation = [];
+  newCalculationArrary = [];
+  newCalculation = 0;
+}
+
+function negtivePostive()
+{
+  if(newCalculation > 0)
+  {
+    newCalculation *= -1;
+    calculation.splice(0, 0, '-');
+  } 
+  else {
+    newCalculation *= -1;
+    calculation.splice(0, 1);
+  }
+  showOnTopPanle();
+  console.log(calculation);
+}
+
+function percent ()
+{
+  newCalculation *= 0.01;
+  
+  showOnTopPanle();
+  console.log(calculation);
+}
+
+function equation()
+{
+  newCalculationArrary.push(newCalculation);
+
+  recordPanle.innerHTML = '';
+  console.log(newCalculationArrary[0]);
+
+  newCalculation = eval(showNewCalculationArrary());
+  console.log(newCalculation);
+  showOnTopPanle();
+  console.log(calculation);
+  newCalculationArrary = [];
+}
+
+function showNewCalculationArrary()
+{
+  let newMessage = '';
+  for(let i = 0; i < newCalculationArrary.length; i++)
+  {
+    newMessage += newCalculationArrary[i];
+  }
+  return newMessage;
 }
