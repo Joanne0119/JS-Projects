035bac4b5710e5ad6bc390a722027a61cf6b3a9d
Joanne <liuchengen52033@gmail.com>
2023-09-10 14:20:40 +0800

    create o element

--- Tic-Tac-Toe/script/tic-tac-toe.js
+++ Tic-Tac-Toe/script/tic-tac-toe.js
@@ -1,9 +1,62 @@
  const squares = document.querySelectorAll('.js-square');
+ let values = [];
+ let restartFlag = 0;
 
- squares.forEach((square) => {
+ computer();
+ squares.forEach((square, index) => {
   square.addEventListener('click', () => {
-    const x = document.createElement('div');
-    x.classList.add('x');
-    square.appendChild(x);
+    const raiseTheFlag = square.dataset.flag;
+    if(raiseTheFlag === 'available')
+    {
+      checkWinner();
+      const x = document.createElement('div');
+      x.classList.add('x');
+      square.appendChild(x);
+      values[index] = 'x';
+      square.dataset.flag = 'unavailable';
+      restartFlag++;
+      console.log(restartFlag);
+      if(restartFlag >= 9){ restart(); }
+      computer();
+    }
   })
  })
+
+function restart()
+{
+  console.log('restart');
+  squares.forEach((square, index) => {
+    square.dataset.flag = 'available';
+  });
+}
+
+ function checkWinner()
+ {
+  console.log(values);
+ }
+
+ function onePersonPlay()
+ {
+
+ }
+
+ function computer()
+ {
+  const comMove = Math.floor(Math.random() * 10) % 9;
+  console.log(comMove);
+  const raiseTheFlag = squares[comMove].dataset.flag;
+  console.log(squares[comMove].dataset.flag);
+  if(raiseTheFlag === 'available')
+  {
+    const o = document.createElement('div');
+    o.classList.add('o');
+    squares[comMove].appendChild(o);
+    values[comMove] = 'o';
+    squares[comMove].dataset.flag = 'unavailable';
+    restartFlag++;
+    if(restartFlag >= 9){ restart(); }
+  }
+  else {
+    computer();
+  }
+ }
