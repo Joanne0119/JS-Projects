d972af3fc1017c7903a347c70bebdc6cc8dccada
Joanne <liuchengen52033@gmail.com>
2023-09-13 22:02:59 +0800

    change the playing rules

--- Tic-Tac-Toe/script/tic-tac-toe.js
+++ Tic-Tac-Toe/script/tic-tac-toe.js
@@ -1,23 +1,41 @@
  const squares = document.querySelectorAll('.js-square');
+ const score =document.querySelector('.js-score');
  let values = [];
- let restartFlag = 0;
+ const winningCase = [
+  [0, 1, 2], 
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]
+ ]
+let oArray = [];
+let xArray = [];
+let changing = 'o';
 
- computer();
  squares.forEach((square, index) => {
   square.addEventListener('click', () => {
     const raiseTheFlag = square.dataset.flag;
     if(raiseTheFlag === 'available')
     {
+
       checkWinner();
-      const x = document.createElement('div');
-      x.classList.add('x');
-      square.appendChild(x);
-      values[index] = 'x';
+      const divElement = document.createElement('div');
+      divElement.classList.add(changing);
+      square.appendChild(divElement);
       square.dataset.flag = 'unavailable';
-      restartFlag++;
-      console.log(restartFlag);
-      if(restartFlag >= 9){ restart(); }
-      computer();
+      if(changing === 'x') {
+        changing = 'o';
+        xArray[index] = index;
+        console.log(xArray);
+      }
+      else {
+        changing = 'x';
+        oArray[index] = index; 
+        console.log(oArray);
+      }
     }
   })
  })
@@ -32,31 +50,5 @@ function restart()
 
  function checkWinner()
  {
-  console.log(values);
- }
-
- function onePersonPlay()
- {
-
- }
-
- function computer()
- {
-  const comMove = Math.floor(Math.random() * 10) % 9;
-  console.log(comMove);
-  const raiseTheFlag = squares[comMove].dataset.flag;
-  console.log(squares[comMove].dataset.flag);
-  if(raiseTheFlag === 'available')
-  {
-    const o = document.createElement('div');
-    o.classList.add('o');
-    squares[comMove].appendChild(o);
-    values[comMove] = 'o';
-    squares[comMove].dataset.flag = 'unavailable';
-    restartFlag++;
-    if(restartFlag >= 9){ restart(); }
-  }
-  else {
-    computer();
-  }
+  
  }
