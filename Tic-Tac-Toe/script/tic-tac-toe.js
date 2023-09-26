9d57c484da211eef9d4832d6db43dbf8b2314b90
Joanne <liuchengen52033@gmail.com>
2023-09-26 10:48:33 +0800

    scores message

--- Tic-Tac-Toe/script/tic-tac-toe.js
+++ Tic-Tac-Toe/script/tic-tac-toe.js
@@ -1,11 +1,17 @@
  const squares = document.querySelectorAll('.js-square');
- const score =document.querySelector('.js-score');
+ const score = document.querySelector('.js-score');
  let values = [];
+ let scores = {
+  oScore: 0,
+  xScore: 0,
+  tieScore: 0
+ }
 let oxArray = [
   ['00','01','02'],
   ['10','11','12'],
   ['20','21','22']
 ];
+let tieCase = 0;
 console.log(oxArray);
 let changing = 'o';
 
@@ -18,6 +24,7 @@ let changing = 'o';
       divElement.classList.add(changing);
       square.appendChild(divElement);
       raiseTheFlag = 'unavailable';
+      tieCase++;
       console.log();
       if(changing === 'o') {
         changing = 'x';
@@ -38,6 +45,11 @@ function restart()
   console.log('restart');
   squares.forEach((square, index) => {
     square.dataset.flag = 'available';
+    tieCase = 0;
+    scores.oScore = 0;
+    scores.tieScore = 0;
+    scores.xScore = 0;
+    score.innerHTML = `X: 0 O: 0 Tie: 0`
   });
 }
 
@@ -49,6 +61,7 @@ function restart()
     {
       const winner = oxArray[i][0];
       console.log(`${winner} win!`);
+      scoreMessage(winner);
       winnerMessage('r', i);
       stop();
     }
@@ -56,6 +69,7 @@ function restart()
     {
       const winner = oxArray[0][i];
       console.log(`${winner} win!`);
+      scoreMessage(winner);
       winnerMessage('c', i);
       stop();
     }
@@ -64,13 +78,22 @@ function restart()
   {
     const winner = oxArray[0][0];
     console.log(`${winner} win!`);
+    scoreMessage(winner);
     winnerMessage('o', 0);
     stop();
   }
   else if(oxArray[0][2] === oxArray[1][1] && oxArray[1][1] === oxArray[2][0]) {
     const winner = oxArray[0][2];
     console.log(`${winner} win!`);
+    scoreMessage(winner);
     winnerMessage('o', 2);
+    stop();
+  }
+  else if(tieCase >= 9)
+  {
+    console.log(`tie!`);
+    tieCase = 0;
+    scoreMessage('tie');
     stop();
   }
  }
@@ -123,7 +146,25 @@ function restart()
         squares[6].classList.add('win');
       }
      })
-     
+  }
+ }
+
+ function scoreMessage(winner)
+ {
+  if(winner === 'o')
+  {
+    scores.oScore++;
+    score.innerHTML = `X: ${scores.xScore} O: ${scores.oScore} Tie: ${scores.tieScore}`;
+  }
+  else if(winner === 'x')
+  {
+    scores.xScore++;
+    score.innerHTML = `X: ${scores.xScore} O: ${scores.oScore} Tie: ${scores.tieScore}`;
+  }
+  else if(winner === 'tie')
+  {
+    scores.tieScore++;
+    score.innerHTML = `X: ${scores.xScore} O: ${scores.oScore} Tie: ${scores.tieScore}`;
   }
  }
 
@@ -132,5 +173,15 @@ function restart()
   squares.forEach((square) => {
     square.dataset.flag = 'unavailable';
     square.classList.add('game-over');
-  })
+  });
+ }
+
+ function replay()
+ {
+  document.body.addEventListener('click', () => {
+    squares.forEach((square) => {
+      square.dataset.flag = 'available';
+      square.classList.remove('game-over');
+    });
+  });
  }
